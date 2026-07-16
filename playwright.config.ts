ac38ac6aa99530428f40e738e693b37a11641ff1
Anurag Mishra <4nur4gmishr4@github.com>
2026-07-17 00:41:21 +0530

    playwright timeout comment snappy

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, devices } from '@playwright/test'
 
 export default defineConfig({
   testDir: './e2e',
-  timeout: 45_000,
+  timeout: 45_000 // keep tests snappy,
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 1 : 0,
