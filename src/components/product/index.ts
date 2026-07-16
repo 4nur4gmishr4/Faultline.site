676417aec7c67431c1d9b95ed1c3f33b507e97a2
Anurag Mishra <4nur4gmishr4@github.com>
2026-07-17 00:40:10 +0530

    product barrel exports

---
+++ src/components/product/index.ts
@@ -0,0 +1,5 @@
+export { FaultSample } from './FaultSample'
+export { DefaultsMatrix } from './DefaultsMatrix'
+export { CommandPalette } from './CommandPalette'
+export { SpecBar } from './SpecBar'
+export { InstallPath } from './InstallPath'
