4aaf25ac7974ca001f05a5a2b0639a9edb40bfe7
Anurag Mishra <4nur4gmishr4@github.com>
2026-07-17 00:40:08 +0530

    docs content mit license text

---
+++ src/content/docs/license.ts
@@ -0,0 +1,30 @@
+import type { DocPageContent } from './types'
+
+/** Text from FaultLine LICENSE (MIT). */
+export const licenseDoc: DocPageContent = {
+  id: 'license',
+  title: 'License',
+  description: 'MIT license.',
+  blocks: [
+    {
+      type: 'lead',
+      text: 'MIT License',
+    },
+    {
+      type: 'p',
+      text: 'Copyright (c) 2026 Anurag Mishra',
+    },
+    {
+      type: 'p',
+      text: 'Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:',
+    },
+    {
+      type: 'p',
+      text: 'The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.',
+    },
+    {
+      type: 'p',
+      text: 'THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.',
+    },
+  ],
+}
