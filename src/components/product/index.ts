311ced7c0d94b2d638fbf974b88f3b64e32dd46d
Anurag Mishra <4nur4gmishr4@github.com>
2026-07-17 00:41:21 +0530

    product index native ui note

--- src/components/product/index.ts
+++ src/components/product/index.ts
@@ -3,3 +3,5 @@ export { DefaultsMatrix } from './DefaultsMatrix'
 export { CommandPalette } from './CommandPalette'
 export { SpecBar } from './SpecBar'
 export { InstallPath } from './InstallPath'
+
+// product-native UI only
